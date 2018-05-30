721fef6ed3638164e83267b5c42b1a325a94b26f
rpidugu <reds.j2ee@gmail.com>
2018-05-31 00:23:59 +0200

    Hystrix command added. Hystrix provides the fallback service for the application.

--- src/main/java/com/javaclix/microservices/helloclient/HelloResource.java
+++ src/main/java/com/javaclix/microservices/helloclient/HelloResource.java
@@ -1,7 +1,7 @@
 package com.javaclix.microservices.helloclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +14,18 @@ public class HelloResource {
     @Autowired
     private RestTemplate restTemplate;
 
-
+    @HystrixCommand(fallbackMethod = "fallback", groupKey = "Hello",
+            commandKey = "hello",
+            threadPoolKey = "helloThread"
+    )
     @GetMapping
     public String getHello(){
         String url = "http://hello-server/rest/hello/server";
         return restTemplate.getForObject(url, String.class);
     }
 
+    //The return type of this method must be the same as the getHello() method.
+    public String fallback(Throwable hystrixCommand) {
+        return "Fall Back Hello world";
+    }
 }
